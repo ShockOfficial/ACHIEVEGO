3b2c20ef07a8427d4a67b2ff4cf1f55f848e60ac
Paul Hoe <pglmotyka@gmial.com>
2020-08-19 01:00:09 +0200

    End of example layout

--- app/src/main/java/com/example/achivgo/MainActivity.java
+++ app/src/main/java/com/example/achivgo/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.achivgo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
